d5a2c46f04a5124fb3bc15a409a40c70e1a1c3d0
Tammy Kay Hansen <94862936+tammykayhansen@users.noreply.github.com>
2021-12-06 16:06:05 -0800

    Update calculator_RPNlogic.js

--- calculator_RPNlogic.js
+++ calculator_RPNlogic.js
@@ -2,6 +2,15 @@
 CSD 122 JavaScript Final Project
 
 Author: Tammy Hansen
+
+Project: RPN Logic Calculator
+   - Logic is similar to HP-48GX calculators (postfix)
+   - Accepts user input string of numbers and calculator operations
+   - Uses an input array created from user input, split on spaces
+   - Switch/case for individual operations
+   - Operations carried out via push(), pop(), and Math functions
+   - Two test cases in comments at end
+   - More details available in project ppt presentation
 */
 
 const readline = require('readline-sync');
